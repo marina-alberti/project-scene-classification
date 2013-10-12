08982a32eef75c8bba13c1a5fa190a127e5af154
marina <marina@marina-HP-ENVY-dv6.(none)>
2013-10-12 11:40:49 +0200

    Adding other files for 2 new classes AllFeatPairObject and AllFeatSingleObject.

---
+++ src/CMakeFiles/AllFeatPairObject.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/marina/Project_Scene_Classification/project-scene-classification/src/AllFeatPairObject.cpp" "/home/marina/Project_Scene_Classification/project-scene-classification/src/CMakeFiles/AllFeatPairObject.dir/AllFeatPairObject.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "DISABLE_OPENNI"
+  "DISABLE_OPENNI"
+  "DISABLE_OPENNI"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/marina/Project_Scene_Classification/project-scene-classification/src/CMakeFiles/FeatureInformation.dir/DependInfo.cmake"
+  )
